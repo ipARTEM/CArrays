ebe84db72eb88f7b409a5add6c67692a72625741
Artem <a.khimin@yandex.ru>
2021-05-15 10:44:48 +0300

    Добавьте файлы проекта.

---
+++ CArrays/CArrays.c
@@ -0,0 +1,27 @@
+﻿
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define ARRAY_LENGTH 10
+#define NUMBERS_AMOUNT 1000000
+
+
+int main()
+{
+	srand(time(NULL));
+	int frequency[ARRAY_LENGTH] = { 0 };
+	int a, i;
+	for ( i = 0; i < NUMBERS_AMOUNT; i++)
+	{
+		a = rand() % ARRAY_LENGTH;
+		frequency[a]++;
+	}
+
+	for ( i = 0; i < ARRAY_LENGTH; i++)
+	{
+		printf("Number %d generated %6d (%5.2f%%) times \n", i, frequency[i], ((float)frequency[i] / NUMBERS_AMOUNT * 100));
+
+	}
+	
+	return 0;
+}
